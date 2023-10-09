6e4297321a476beb371b9cde5d6b870b705a14b8
AlbertoDPM30 <albertodpm2000@gmail.com>
2023-10-09 03:27:13 -0400

    add baseURL: http://localhost:3000/api & new build

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //baseURL: "http://localhost:3000/api",
+  baseURL: "http://localhost:3000/api",
   //baseURL: "http://3.20.40.119/api",
   //baseURL: "https://adpmtasksbackend.onrender.com/api",
   withCredentials: true,
